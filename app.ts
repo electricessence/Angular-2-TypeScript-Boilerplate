35a42b446f798417b46a7de6af2044f7e71d6efd
electricessence <oren@electrified.net>
2015-09-21 08:14:51 -0700

    Initial commit including WebStorm TS compiler options.

---
+++ app.ts
@@ -0,0 +1,27 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+// Annotation section
+@Component(
+	{
+		selector: 'my-app'
+	}
+)
+@View(
+	{
+		template: '<h1>Hello {{ name }}.  How are you.</h1>'
+	}
+)
+	// Component controller
+class MyAppComponent
+{
+	name:string;
+
+	constructor()
+	{
+		this.name = 'Bob';
+	}
+}
+
+bootstrap(MyAppComponent);
